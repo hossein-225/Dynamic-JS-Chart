f48d96646db3e45d87408aec9e64a2f57f52b889
hossein_225 <hossein_225@yahoo.com>
2022-11-21 19:47:46 +0330

    change src folder

--- src/script.js
+++ src/script.js
@@ -1,16 +1,82 @@
+data = [10, 90, 20, 30, 60];
+categories = [1, 2, 3, 4, 5];
+
 var options = {
+  series: [
+    {
+      data: data.slice(),
+    },
+  ],
   chart: {
-    type: 'bar'
+    id: "realtime",
+    height: 350,
+    type: "line",
+    animations: {
+      enabled: true,
+      easing: "linear",
+      dynamicAnimation: {
+        speed: 1000,
+      },
+    },
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
   },
-  series: [{
-    name: 'sales',
-    data: [30,40,45,50,49,60,70,91,125]
-  }],
+  ataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  title: {
+    text: "Dynamic Updating Chart",
+    align: "left",
+  },
+  markers: {
+    size: 0,
+  },
   xaxis: {
-    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-  }
-}
+    categories: categories,
+  },
+  yaxis: {
+    max: 100,
+  },
+  legend: {
+    show: false,
+  },
+};
 
 var chart = new ApexCharts(document.querySelector("#chart"), options);
 
 chart.render();
+
+window.setInterval(function () {
+  getRandomIntData(10, 90);
+  getRandomIntcategories(5, 100);
+
+  chart.updateSeries([
+    {
+      data: data,
+      categories: categories,
+    },
+  ]);
+}, 1000);
+
+function getRandomIntData(min, max) {
+  data.shift();
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  data.push(Math.floor(Math.random() * (max - min) + min));
+  return data;
+}
+
+function getRandomIntcategories(min, max) {
+  categories.shift();
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  categories.push(Math.floor(Math.random() * (max - min) + min));
+  return categories;
+}
